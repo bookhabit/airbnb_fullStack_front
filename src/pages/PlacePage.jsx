462b25f9251fbda830f252b0c21d65cbcba06f31
bookhabit <qnrgoqlt@naver.com>
2023-06-11 20:45:19 +0900

    숙소상세페이지

--- src/pages/PlacePage.jsx
+++ src/pages/PlacePage.jsx
@@ -1,47 +1,99 @@
-import {useContext, useState} from "react";
-import {UserContext} from "../UserContext.jsx";
-import {Link, Navigate, useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
 import axios from "axios";
-import PlacesPage from "./PlacesPage";
-import AccountNav from "../AccountNav";
+import Image from "../Image";
+import BookingWidget from "../BookingWidget";
 
-export default function ProfilePage() {
-  const [redirect,setRedirect] = useState(null);
-  const {ready,user,setUser} = useContext(UserContext);
-  let {subpage} = useParams();
-  if (subpage === undefined) {
-    subpage = 'profile';
-  }
+export default function PlacePage() {
+  const {id} = useParams();
+  const [place,setPlace] = useState(null);
+  const [showAllPhotos,setShowAllPhotos] = useState(false)
+  useEffect(()=>{
+    if(!id){
+      return;
+    }
+    axios.get(`/places/${id}`).then(response=>{
+      setPlace(response.data)
+    })
+  },[id])
 
-  async function logout() {
-    await axios.post('/logout');
-    setRedirect('/');
-    setUser(null);
-  }
+  if (!place) return '숙소 불러오지 못함';
 
-  if (!ready) {
-    return 'Loading...';
-  }
-
-  if (ready && !user && !redirect) {
-    return <Navigate to={'/login'} />
-  }
-
-  if (redirect) {
-    return <Navigate to={redirect} />
-  }
-  return (
-    <div>
-      <AccountNav />
-      {subpage === 'profile' && (
-        <div className="text-center max-w-lg mx-auto">
-          Logged in as {user.name} ({user.email})<br />
-          <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+  if (showAllPhotos) {
+    return (
+      <div className="absolute inset-0 bg-black text-white min-h-screen">
+        <div className="bg-black p-8 grid gap-4">
+          <div>
+            <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
+            <button onClick={() => setShowAllPhotos(false)} className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
+              </svg>
+              Close photos
+            </button>
+          </div>
+          {place?.photos?.length > 0 && place.photos.map(photo => (
+            <div>
+              <Image src={photo} alt=""/>
+            </div>
+          ))}
         </div>
-      )}
-      {subpage === 'places' && (
-        <PlacesPage />
-      )}
+      </div>
+    );
+  }
+
+  return (
+    <div className="mt-4 bg-gray-100 -mx-8 px-8 py-8">
+      <h1 className="text-3xl">{place.title}</h1>
+      <a className="block font-semibold underline my-2" target="_blank" href={`https://maps.google.com/?q=${place.address}`}>{place.address}</a>
+      <div className="relative">
+        <div className="grid gap-2 grid-cols-[2fr_1fr] rounded-3xl overflow-hidden
+        ">
+          <div>
+            {place.photos?.[0]&&(
+              <div key={place.id}>
+                <img onClick={()=>setShowAllPhotos(true)} className="aspect-square cursor-pointer object-cover" src={'http://localhost:4000/uploads/'+place.photos?.[0]}alt="이미지"/>
+              </div>
+            )}
+          </div>
+          <div className="grid">  
+            {place.photos?.[1]&&(
+                <img onClick={()=>setShowAllPhotos(true)} className="aspect-square cursor-pointer" src={'http://localhost:4000/uploads/'+place.photos?.[1]}alt="이미지"/>
+            )}
+            <div className="overflow-hidden">
+              {place.photos?.[2]&&(
+                  <img onClick={()=>setShowAllPhotos(true)} className="aspect-square cursor-pointer relative top-2" src={'http://localhost:4000/uploads/'+place.photos?.[2]}alt="이미지"/>
+              )}
+            </div>
+          </div>
+        </div>
+        <button className="absolute flex gap-1 bottom-2 right-2 py-2 px-4 bg-white rounded-2xl shadow shadow-gray-500" onClick={()=>setShowAllPhotos(true)}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+        </svg>
+          Show more photos
+        </button>
+      </div>
+      <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
+        <div>
+          <div className="my-4">
+            <h2 className="font-semibold text-2xl">Description</h2>
+            {place.description}
+          </div>
+          Check-in: {place.checkIn}<br />
+          Check-out: {place.checkOut}<br />
+          Max number of guests: {place.maxGuests}
+        </div>
+        <div>
+          <BookingWidget place={place}/>
+        </div>
+      </div>
+      <div className="bg-white -mx-8 px-8 py-8 border-t">
+        <div>
+          <h2 className="font-semibold text-2xl">Extra info</h2>
+        </div>
+        <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">{place.extraInfo}</div>
+      </div>
     </div>
   );
 }
