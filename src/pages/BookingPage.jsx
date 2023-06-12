3f37d2643cea2e5150ab14ccac60cdf24a824ee8
bookhabit <qnrgoqlt@naver.com>
2023-06-12 16:09:20 +0900

    placeImage컴포넌트 분리

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,7 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router"
 
 export default function BookingPage(){
     const {id} = useParams();
+    const [booking,setBooking]=useState(null)
+    useEffect(() => {
+        if (id) {
+          axios.get('/bookings').then(response => {
+            const foundBooking = response.data.find(({_id}) => _id === id);
+            if (foundBooking) {
+              setBooking(foundBooking);
+            }
+          });
+        }
+      }, [id]);
+    
+      if (!booking) {
+        return '';
+      }
+      console.log(booking)
     return(
         <div>single booking {id}</div>
     )
